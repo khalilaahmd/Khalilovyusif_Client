a95d63461634d2863cc13e662a61db954e154e65
Khalil <khalilaahmd@gmail.com>
2023-10-25 02:04:00 +0200

    update 25-10-23

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
 import '../Styling/HomePage.css';
-import facebookIcon from '../Images/Others/Facebook.png';
-import instagramIcon from '../Images/Others/Instagram.png';
+import facebookIcon from '../Images/Others/facebook.png';
+import instagramIcon from '../Images/Others/instagram.png';
+import pinterestIcon from '../Images/Others/pinterest.png';
+import behanceIcon from '../Images/Others/behance.png';
+import linkedinIcon from '../Images/Others/linkedin.png';
+import tiktokIcon from '../Images/Others/tik-tok.png';
+
 import { Link } from 'react-router-dom';
 
 function Footer () {
@@ -14,10 +19,26 @@ function Footer () {
     <div className="social-media-container">
         <a href="https://www.facebook.com/khalilovyusif" target="_blank" rel="noopener noreferrer">
         <img src={facebookIcon} alt="Facebook" />
-                     </a>
+        </a>
 
         <a href="https://www.instagram.com/khalilovyusif" target="_blank" rel="noopener noreferrer">
         <img src={instagramIcon} alt="Instagram" />
+        </a>
+
+        <a href="https://nl.pinterest.com/khalilovyusif/" target="_blank" rel="noopener noreferrer">
+        <img src={pinterestIcon} alt="Pinterest" />
+        </a>
+
+        <a href="https://www.behance.net/khalilovyusif" target="_blank" rel="noopener noreferrer">
+        <img src={behanceIcon} alt="Behance" />
+        </a>
+
+        <a href="https://www.linkedin.com/in/khalilovyusif/" target="_blank" rel="noopener noreferrer">
+        <img src={linkedinIcon} alt="LinkedIn" />
+        </a>
+
+        <a href="https://www.tiktok.com/@khalilov_yusif" target="_blank" rel="noopener noreferrer">
+        <img src={tiktokIcon} alt="Tiktok" />
         </a>
                      
     </div>
